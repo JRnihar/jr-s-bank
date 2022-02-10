1fe69d789b67774a06cf97ff5ebc458c1b26563c
unknown <niharmonirulislam@gmail.com>
2022-02-10 11:59:52 +0600

    hello

--- js/tk.js
+++ js/tk.js
@@ -9,5 +9,46 @@ document.getElementById('deposite-button').addEventListener('click', function() 
     let preDepoAmount = parseFloat(preDepoAmountText);
     let newDepoTotal = preDepoAmount + newDepoAmount;
     depoTotal.innerText = newDepoTotal;
+
+    // handle balance
+
+    let prevTotal= document.getElementById('balance-total')
+    let prevBalanceText= prevTotal.innerText;
+    let prevBalanceAmount= parseFloat(prevBalanceText);
+    let newBalanceTotal=prevBalanceAmount + newDepoAmount;
+    prevTotal.innerText=newBalanceTotal;
+
+ 
     depoInput.value = " ";
 })
+
+
+document.getElementById('withdraw-button').addEventListener('click', function() {
+    let withDrawInput = document.getElementById('withdraw-input');
+
+ 
+    let newWithDrawAmountText =  withDrawInput.value;
+    let newWithDrawAmount = parseFloat( newWithDrawAmountText);
+    
+
+    let withDrawTotal = document.getElementById('with-total');
+    let preWithDrawAmountText = withDrawTotal.innerText;
+
+   
+    let preWithDrawAmount = parseFloat( preWithDrawAmountText);
+
+    let newWithDrawTotal =  preWithDrawAmount +newWithDrawAmount;
+    withDrawTotal.innerText = newWithDrawTotal;
+
+    //handle balance
+
+    let prevTotal= document.getElementById('balance-total')
+    let prevBalanceText= prevTotal.innerText;
+    let prevBalanceAmount= parseFloat(prevBalanceText);
+    let newBalanceTotal=prevBalanceAmount -  newWithDrawAmount;
+    prevTotal.innerText=newBalanceTotal;
+
+ 
+    withDrawInput.value = " ";
+})
+
