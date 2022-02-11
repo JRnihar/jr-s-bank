4d748df422e6c7b51a57fc9067918196fc403ab5
JRnihar <niharmonirulislam@gmail.com>
2022-02-11 18:43:56 +0600

    hello3

--- js/tk.js
+++ js/tk.js
@@ -1,54 +1,56 @@
+function getInput(Input) {
+    let inputField = document.getElementById(Input);
+    let inputAmounttext = inputField.value;
+    let amountvalue = parseFloat(inputAmounttext);
+    inputField.value = " ";
+    return amountvalue;
+}
+
+function updateField(totalFieldId, Amount) {
+    let totalElement = document.getElementById(totalFieldId);
+    let totalText = totalElement.innerText;
+    let preTotal = parseFloat(totalText);
+    totalElement.innerText = preTotal + Amount;
+}
+
+function getCurrentBalance() {
+    let prevTotal = document.getElementById('balance-total')
+    let prevBalanceText = prevTotal.innerText;
+    let prevBalanceAmount = parseFloat(prevBalanceText);
+    return prevBalanceAmount;
+}
+
+function updateBlance(amount, isAdd) {
+    let prevTotal = document.getElementById('balance-total')
+    const prevBalanceAmount = getCurrentBalance()
+    if (isAdd == true) {
+        prevTotal.innerText = prevBalanceAmount + amount;
+    } else {
+        prevTotal.innerText = prevBalanceAmount - amount;
+    }
+}
+
+
+
 document.getElementById('deposite-button').addEventListener('click', function() {
-    let depoInput = document.getElementById('deposite-input');
-    let newDepoAmountText = depoInput.value;
-    let newDepoAmount = parseFloat(newDepoAmountText);
-    // console.log(depoAmount);
+    const newDepoAmount = getInput('deposite-input');
 
-    let depoTotal = document.getElementById('depo-total');
-    let preDepoAmountText = depoTotal.innerText;
-    let preDepoAmount = parseFloat(preDepoAmountText);
-    let newDepoTotal = preDepoAmount + newDepoAmount;
-    depoTotal.innerText = newDepoTotal;
+    if (newDepoAmount > 0) {
+        updateField('depo-total', newDepoAmount);
+        updateBlance(newDepoAmount, true);
+    }
 
-    // handle balance
-
-    let prevTotal= document.getElementById('balance-total')
-    let prevBalanceText= prevTotal.innerText;
-    let prevBalanceAmount= parseFloat(prevBalanceText);
-    let newBalanceTotal=prevBalanceAmount + newDepoAmount;
-    prevTotal.innerText=newBalanceTotal;
-
- 
-    depoInput.value = " ";
 })
 
 
 document.getElementById('withdraw-button').addEventListener('click', function() {
-    let withDrawInput = document.getElementById('withdraw-input');
+    const newWithDrawAmount = getInput('withdraw-input');
 
- 
-    let newWithDrawAmountText =  withDrawInput.value;
-    let newWithDrawAmount = parseFloat( newWithDrawAmountText);
-    
 
-    let withDrawTotal = document.getElementById('with-total');
-    let preWithDrawAmountText = withDrawTotal.innerText;
+    const currentBalance = getCurrentBalance();
+    if (newWithDrawAmount > 0 && newWithDrawAmount < currentBalance) {
+        updateField('with-total', newWithDrawAmount);
+        updateBlance(newWithDrawAmount, false);
+    }
 
-   
-    let preWithDrawAmount = parseFloat( preWithDrawAmountText);
-
-    let newWithDrawTotal =  preWithDrawAmount +newWithDrawAmount;
-    withDrawTotal.innerText = newWithDrawTotal;
-
-    //handle balance
-
-    let prevTotal= document.getElementById('balance-total')
-    let prevBalanceText= prevTotal.innerText;
-    let prevBalanceAmount= parseFloat(prevBalanceText);
-    let newBalanceTotal=prevBalanceAmount -  newWithDrawAmount;
-    prevTotal.innerText=newBalanceTotal;
-
- 
-    withDrawInput.value = " ";
 })
-
